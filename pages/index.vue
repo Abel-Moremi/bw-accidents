3427f3e1a595766692f70c07244d2b71bdb35eb0
Abel Moremi <abelmoremiz@gmail.com>
2021-05-24 21:29:18 +0200

    add: dashboard base UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,42 @@
 <template>
-  <div>
-    <donut-chart-component />
+
+  <div class="flex min-h-screen bg-primary-50">
+    <div class="bg-white py-4 px-3 md:py-6 md:px-8">
+      <nav>
+        <ul class="py-4">
+          <li class="py-2">
+            <nuxt-link to="/" class="nav-link">
+              <!-- icon svg -->
+            </nuxt-link>
+          </li>
+          <li class="py-2">
+            <nuxt-link to="/styleguide" class="nav-link">
+              <!-- icon svg -->
+            </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="flex-auto h-screen overflow-y-auto p-6" />
   </div>
 </template>
 
 <script>
-import DonutChartComponent from '~/components/DonutChartComponent.vue'
 export default {
-  components: { DonutChartComponent },
+  
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+<style scoped>
+.nav-link {
+  @apply transition-all duration-1000 ease-in-out block w-10 h-10 bg-white text-primary-400 rounded-full flex items-center justify-center shadow-inner;
+
+  @screen md {
+    @apply w-12 h-12;
+  }
 }
-*/
+.nav-link:hover,
+.nuxt-link-exact-active {
+  @apply bg-primary-400 text-white shadow-lg;
+}
 </style>
