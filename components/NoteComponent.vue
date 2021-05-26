4a9ba5cb5330a0f4463c40cfdc04f3036e9c4d55
Abel Moremi <abelmoremiz@gmail.com>
2021-05-26 03:24:51 +0200

    add: NoteComponent.vue

---
+++ components/NoteComponent.vue
@@ -0,0 +1,23 @@
+<template> 
+    <section class="flex shadow">
+    <!-- card container -->
+        <div class="rounded overflow-hidden m-4 sm:flex bg-white">  
+            <!-- card-content -->
+            <div class="px-6 py-4">
+                <h2 class="mb-2 font-black">
+                    Hello Tailwind!
+                </h2>
+                <p class="mb-4 text-grey-dark text-sm">
+                    Learning Tailwind is incredibly easy. 
+                    The team has done a wonderful job with 
+                    the documentation. This is pretty amazing, I must say.
+                </p> 
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
